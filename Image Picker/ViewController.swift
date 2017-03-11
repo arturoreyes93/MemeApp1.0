38a5936de1a751b249fd6ee36968ae05554755fa
Arturo Reyes <reyesm93@gmail.com>
2017-03-11 12:58:59 -0600

    set up storyboard

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func pickImage(_ sender: Any) {
+        
+        let pickerController = UIImagePickerController()
+        self.present(pickerController, animated: true, completion: nil)
     }
     
     
