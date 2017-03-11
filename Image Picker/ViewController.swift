747d3242f1b9d1a91a0eaa10b4ab8f9d68f29cfc
Arturo Reyes <reyesm93@gmail.com>
2017-03-10 19:36:29 -0600

    add image view in vc

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -15,14 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func experiment() {
-        
-        let nextController = UIImagePickerController()
-        
-        self.present(nextController, animated: true, completion: nil)
-        
-    }
-
+    
 
 }
 
