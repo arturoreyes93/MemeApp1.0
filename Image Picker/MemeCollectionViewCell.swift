c409cf3dbf0bffb8df3ee6c60ea479df9248eb15
Arturo Reyes <reyesm93@gmail.com>
2017-04-22 17:53:52 -0500

    Create ViewControllers and Classes for Collection and Tables

---
+++ Image Picker/MemeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCollectionViewCell.swift
+//  Image Picker
+//
+//  Created by Arturo Reyes on 4/22/17.
+//  Copyright © 2017 Arturo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeCellImage: UIImageView!
+    
+    
+    
+}
